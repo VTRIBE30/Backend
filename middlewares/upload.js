fd051b4d67c64bc80adf36d1496e31b085fe4161
David Horjet <davidojetola956@gmail.com>
2024-09-03 18:18:46 +0100

    chore: updating multer validation

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,32 +1,13 @@
 const multer = require("multer");
-const path = require("path");
 
 const storage = multer.diskStorage({
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "_" + file.originalname);
+    return cb(null, Date.now() + "_" + file.originalname);
   },
 });
 
-function fileFilter(req, file, cb) {
-  const filetypes = /jpeg|jpg|png|mp4/; // Allowed file types
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    return cb(
-      new multer.MulterError(
-        "LIMIT_UNEXPECTED_FILE",
-        "Invalid file type. Only 'jpeg, jpg, png, mp4' are allowed!"
-      )
-    );
-  }
-}
-
 const upload = multer({
   storage: storage,
-  fileFilter: fileFilter,
   limits: { fileSize: 1024 * 1024 * 10 },
 });
 
